cf5a3e6bdb8fe3f3e4905642113ddb3d4bb56309
tjodalv2k <svennsorensen@gmail.com>
2017-04-03 13:42:03 +0200

    framework and start page

---
+++ api/controllers/UserController.js
@@ -0,0 +1,16 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+
+  'new': function (req, res) {
+    res.view();
+  },
+
+};
+
---
+++ api/models/User.js
@@ -0,0 +1,27 @@
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    email: {
+    type: 'email',
+    required: true
+    },
+
+    password: {
+    type: 'string',
+    required: true
+    }
+  }
+
+
+
+
+};
+
