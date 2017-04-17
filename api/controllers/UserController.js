6abbc81cdf76f8742b0475f34cb939f50a007028
mortea15 <mortea15@student.uia.no>
2017-04-17 19:39:56 +0200

    Rewriting of UserController, allowing authentication

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -8,31 +8,72 @@
 module.exports = {
 
 
-  'new': function (req, res) {
-    res.view();
-  },
 
-  'login': function (emailField, passwordField, req, res, next) {
-    User.find({
-      and: [
-        {email: emailField},
-        {password: passwordField}
-      ]
-    }).exec(function foundUser(err, user) {
-      if (err) return next(err);
+  /**
+   * `UserController.login()`
+   */
+  login: function (req, res) {
 
-      res.view({
-        user: user
-      });
+    // See `api/responses/login.js`
+    return res.login({
+      email: req.param('email'),
+      password: req.param('password'),
+      successRedirect: '/',
+      invalidRedirect: '/login'
     });
   },
 
-  create: function (req, res, next) {
-    User.create(req.params.all(), function userCreated(err, user) {
-      if (err) return next(err);
 
-      //res.json(user);
-      res.redirect('/user/show/' + user.id);
+  /**
+   * `UserController.logout()`
+   */
+  logout: function (req, res) {
+
+    // "Forget" the user from the session.
+    // Subsequent requests from this user agent will NOT have `req.session.me`.
+    req.session.me = null;
+
+    // If this is not an HTML-wanting browser, e.g. AJAX/sockets/cURL/etc.,
+    // send a simple response letting the user agent know they were logged out
+    // successfully.
+    if (req.wantsJSON) {
+      return res.ok('Logged out successfully!');
+    }
+
+    // Otherwise if this is an HTML-wanting browser, do a redirect.
+    return res.redirect('/');
+  },
+
+
+  /**
+   * `UserController.signup()`
+   */
+  signup: function (req, res) {
+
+    // Attempt to signup a user using the provided parameters
+    User.signup({
+      name: req.param('name'),
+      email: req.param('email'),
+      password: req.param('password')
+    }, function (err, user) {
+      // res.negotiate() will determine if this is a validation error
+      // or some kind of unexpected server error, then call `res.badRequest()`
+      // or `res.serverError()` accordingly.
+      if (err) return res.negotiate(err);
+
+      // Go ahead and log this user in as well.
+      // We do this by "remembering" the user in the session.
+      // Subsequent requests from this user agent will have `req.session.me` set.
+      req.session.me = user.id;
+
+      // If this is not an HTML-wanting browser, e.g. AJAX/sockets/cURL/etc.,
+      // send a 200 response letting the user agent know the signup was successful.
+      if (req.wantsJSON) {
+        return res.ok('Signup successful!');
+      }
+
+      // Otherwise if this is an HTML-wanting browser, redirect to /welcome.
+      return res.redirect('/');
     });
   },
 
@@ -43,16 +84,6 @@ module.exports = {
 
       res.view({
         user: user
-      });
-    });
-  },
-
-  index: function (req, res, next) {
-    User.find(function foundUsers(err, users) {
-      if (err) return next(err);
-
-      res.view({
-        users: users
       });
     });
   },
