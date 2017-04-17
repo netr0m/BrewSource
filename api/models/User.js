94fffe787fd71eed05e54bdf78852cf51e1de815
mortea15 <mortea15@student.uia.no>
2017-04-17 19:40:23 +0200

    Rewriting of User, allowing Authentication

--- api/models/User.js
+++ api/models/User.js
@@ -2,24 +2,20 @@
  * User.js
  *
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
- * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ * @docs        :: http://sailsjs.org/#!documentation/models
  */
 
 module.exports = {
 
   attributes: {
-
     email: {
-    type: 'email',
-    email: true,
-    unique: true
+      type: 'email',
+      required: true
     },
-
     password: {
-    type: 'string',
-    required: true
+      type: 'string',
+      required: true
     },
-
     name: {
       type: 'string',
       required: true
@@ -29,6 +25,50 @@ module.exports = {
       collection: 'brewery',
       via: 'owner'
     }
+  },
+
+
+  /**
+   * Create a new user using the provided inputs,
+   * but encrypt the password first.
+   *
+   * @param  {Object}   inputs
+   *                     • name     {String}
+   *                     • email    {String}
+   *                     • password {String}
+   * @param  {Function} cb
+   */
+
+  signup: function (inputs, cb) {
+    // Create a user
+    User.create({
+      name: inputs.name,
+      email: inputs.email,
+      // TODO: But encrypt the password first
+      password: inputs.password
+    })
+      .exec(cb);
+  },
+
+
+
+  /**
+   * Check validity of a login using the provided inputs.
+   * But encrypt the password first.
+   *
+   * @param  {Object}   inputs
+   *                     • email    {String}
+   *                     • password {String}
+   * @param  {Function} cb
+   */
+
+  attemptLogin: function (inputs, cb) {
+    // Create a user
+    User.findOne({
+      email: inputs.email,
+      // TODO: But encrypt the password first
+      password: inputs.password
+    })
+      .exec(cb);
   }
 };
-
