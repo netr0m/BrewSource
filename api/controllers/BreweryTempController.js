ac6d9f7c1b2850a6b0db5b9186ecfee394f508b6
mortea15 <mortea15@student.uia.no>
2017-06-01 12:35:10 +0200

    Changed the functions of the temperaturecontroller

--- api/controllers/BreweryTempController.js
+++ api/controllers/BreweryTempController.js
@@ -7,62 +7,55 @@
 
 module.exports = {
 
+  /**
+   * This normally refers to a built-in action in blueprints, but we'll
+   * override it to strip some properties before sending the response.
+   * @param req
+   * @param res
+   */
+  findOne: function(req, res) {
+
+    if (!req.param('id')) {
+      return res.badRequest('id is a required parameter');
+    }
+
+    BreweryTemp.findOne(req.param('id')).populateAll().exec(function(err, breweryTemp) {
+      if (err) return res.negotiate(err);
+      if (!breweryTemp) return res.notFound();
+
+      // Send the attributes
+      return res.json({
+        id: breweryTemp.id,
+        temperature: breweryTemp.temperature,
+        createdAt: breweryTemp.createdAt,
+        owner: breweryTemp.owner
+      });
+    });
+  },
+
+  find: function(req, res) {
+    BreweryTemp.find().populateAll().exec(function (err, breweryTemps) {
+      if (err) return res.negotiate(err);
+
+      var prunedBreweryTemps = [];
+
+      // Loop through each breweryTemp
+      _.each(breweryTemps, function(breweryTemp) {
+
+        prunedBreweryTemps.push({
+          id: breweryTemp.id,
+          temperature: breweryTemp.temperature,
+          createdAt: breweryTemp.createdAt,
+          owner: breweryTemp.owner
+        });
+      });
+      // Finally, send array of brewerytemps in response
+      return res.json(prunedBreweryTemps);
+    })
+  },
+
 
   'new': function (req, res) {
     res.view();
   },
-
-  create: function (req, res, next) {
-    BreweryTemp.create(req.params.all(), function breweryTempCreated(err, breweryTemp) {
-      if (err) return next(err);
-
-      res.json(breweryTemp);
-    });
-  },
-
-  show: function (req, res, next) {
-    BreweryTemp.findOne(req.param('id')).populateAll().exec(function (err, breweryTemp) {
-      if (err) return next(err);
-      if (!breweryTemp) return next();
-
-      res.view({
-        breweryTemp: breweryTemp
-      });
-    });
-  },
-
-  index: function (req, res, next) {
-    BreweryTemp.find(function foundBreweryTemps(err, breweryTemps) {
-      if (err) return next(err);
-
-      res.json(breweryTemps);
-    });
-  },
-
-  edit: function (req, res, next) {
-    BreweryTemp.findOne(req.param('id'), function foundBreweryTemp(err, breweryTemp) {
-      if (err) return next(err);
-      if (!breweryTemp) return next();
-
-      res.view({
-        breweryTemp: breweryTemp
-      });
-    });
-  },
-
-  update: function (req, res, next) {
-    BreweryTemp.update(req.param('id'), req.params.all(), function breweryTempUpdated(err) {
-      if (err) {
-        return res.redirect('/breweryTemp/edit/' + req.param('id'));
-      }
-
-      res.redirect('/breweryTemp/show/' + req.param('id'));
-    });
-  },
-
-  destroy: function (req, res, next) {
-    BreweryTemp.destroy(req.param('id')).exec(function () {
-      res.redirect('/breweryTemp/');
-    });
-  }
 };
