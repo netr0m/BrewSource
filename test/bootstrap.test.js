6b8ca2c9fb49705e72ab224041371c60ddea490a
mortea15 <mortea15@student.uia.no>
2017-06-02 19:25:42 +0200

    Test which sets up sails lift

---
+++ test/bootstrap.test.js
@@ -0,0 +1,23 @@
+/**
+ * Created by mortea15 on 02.06.2017.
+ */
+var sails = require('sails');
+
+before(function(done) {
+
+  // Increase the Mocha timeout so that Sails has enough time to lift.
+  this.timeout(5000);
+
+  sails.lift({
+    // configuration for testing purposes
+  }, function(err) {
+    if (err) return done(err);
+    // here you can load fixtures, etc.
+    done(err, sails);
+  });
+});
+
+after(function(done) {
+  // here you can clear fixtures, etc.
+  sails.lower(done);
+});
